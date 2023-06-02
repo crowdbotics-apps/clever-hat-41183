76c6f54a335f1c9dc0f7804e0b51205684b5576a
Crowdbotics <team@crowdbotics.com>
2023-06-02 08:27:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "clever-hat-41183",
+  url: "https://clever-hat-41183.botics.co",
+  api: "https://clever-hat-41183.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
